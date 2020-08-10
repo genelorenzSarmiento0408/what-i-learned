ad280c4296d22a70b9ea51f752aa025357a24b62
genelorenzSarmiento0408 <69145401+genelorenzSarmiento0408@users.noreply.github.com>
2020-08-10 15:47:05 +0800

    whole
    the whole js file

---
+++ i learned on js.js
@@ -0,0 +1,29 @@
+let mommy = 5;
+let score = 0;
+let lorenz = 1;
+let fishing ="papa";
+var SA = 3052452;
+var soundeffects;
+let array = fishing.length
+visiter_name = prompt("Input your name : ")
+
+if(visiter_name != null && visiter_name != "")
+alert("Your Name is : "+visiter_name);
+else 
+alert("No name")
+
+ mommy.addEventListener
+    mommy = false
+     mommy = 4 ;
+    {
+        
+        mommy = true;
+    }
+;
+if (mommy=4) {
+    mommy = false;
+}
+
+else(mommy=4) => {
+    question = false;
+}
