a16484c86cf281a7ea7a54c281a553f91a39023d
genelorenzSarmiento0408 <69145401+genelorenzSarmiento0408@users.noreply.github.com>
2020-09-01 19:09:27 +0800

    Create i learned on c#.cs

---
+++ i learned on c#.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace HelloWorld
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("i learned on unity/ c sharp");
+      Console.Write("i learned on unity is how to develop a game using public class");    
+    }
+  }
+}
+
